99f2a44d3f756589ee69237e09703c5d2266df33
Kevin Wilusz <kwilusz10@gmail.com>
2023-03-09 15:06:03 +0100

    working more later home

--- guess/highscore.py
+++ guess/highscore.py
@@ -10,23 +10,24 @@ class Highscore():
         self.player2 = player2
         self.player3 = player3
         self.player4 = player4
+        global players
         players = {"Player 1": self.player1, "Player 2": self.player2,
                    "Player 3": self.player3, "Player 4": self.player4}
-        
-
-
-    def add_scores(self, player, points):
-        
-    def sort(self, players):      # will be used as refresh
+        # player variables works as medium for the points
+    def add_scores(self, players, points, player):
+        playerpoints = players.get(player)
+        self.player 
+    
+    def sort(self):      # will be used as refresh
         """adds and sorts scores"""
         # Player 3 - 4 will always exist but will be as none if not played
         # This way we can make them invinsible if not played, but still exist
-        player1 = ("Player 1", players.get("Player 1"))
-        player2 = ("Player 2", players.get("Player 2"))
-        player3 = ("Player 3", players.get("Player 3", None))
-        player4 = ("Player 4", players.get("Player 4", None))
+        # player1 = ("Player 1", players.get("Player 1"))
+        # player2 = ("Player 2", players.get("Player 2"))
+        # player3 = ("Player 3", players.get("Player 3", None))
+        # player4 = ("Player 4", players.get("Player 4", None))
         # implementation of simple bubble sort
-        thelist = list(filter(None, [player1, player2, player3, player4])) # filtering None values
+        thelist = list(filter(None, [self.player1, self.player2, self.player3, self.player4])) # filtering None values
         n = len(thelist)
         for i in 4:
             # Last i elements are already sorted
